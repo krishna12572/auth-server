99cb524814c4b7f50e32db8875171274ae929ef3
krishna12572 <kp123pkl@gmail.com>
2026-06-30 06:23:19 +0900

    Update helpers.go

--- graph/helpers.go
+++ graph/helpers.go
@@ -1,36 +1,45 @@
 package graph
 
 import (
-"time"
+	"errors"
+	"fmt"
+	"os"
+	"time"
 
-"github.com/golang-jwt/jwt/v5"
-"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
-var jwtSecret = []byte("mysecret")
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
 func generateToken(userID int) (string, error) {
-token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-"user_id": userID,
-"exp":     time.Now().Add(time.Hour).Unix(),
-})
-return token.SignedString(jwtSecret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour).Unix(),
+	})
+	return token.SignedString(jwtSecret)
 }
 
 func generateRefreshToken() string {
-return uuid.New().String()
+	return uuid.New().String()
 }
 
 func validateToken(tokenStr string) (jwt.MapClaims, error) {
-parsed, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-return jwtSecret, nil
-})
-if err != nil {
-return nil, err
-}
-claims, ok := parsed.Claims.(jwt.MapClaims)
-if !ok {
-return nil, jwt.ErrTokenInvalidClaims
-}
-return claims, nil
+	parsed, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if !parsed.Valid {
+		return nil, errors.New("invalid token")
+	}
+	claims, ok := parsed.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, jwt.ErrTokenInvalidClaims
+	}
+	return claims, nil
 }
